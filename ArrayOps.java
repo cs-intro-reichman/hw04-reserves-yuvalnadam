7688bca3a0a2e581ecf82bcb88bf23d79200f93a
Yuval nadam <Yuval.nadam@post.runi.ac.il>
2024-02-23 07:39:03 +0200

    first

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,169 @@
 public class ArrayOps {
     public static void main(String[] args) {
+
+        int[] arr = {3, 0, 1};
+        int[] arr2 = {5, -4, 3, 1};
+        System.out.println(findMissingInt(arr));
+        System.out.println(secondMaxValue(arr));
+        System.out.println(containsTheSameElements(arr, arr2));
+        System.out.println(isSorted(arr));
+
+
         
     }
+
+    /** This function takes a single parameter: an array of integer values.
+     * The array contains each integer 0 ≤𝑥≤𝑛, when n is the size of the array, exactly once, except for one of these integers, which is missing.
+     * The function returns the missing integer */
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
-    }
 
+        int ln = array.length;
+        int sum = sum(array);
+        int[] fullArr = new int[ln];
+        int count = 0;
+        for (int i = 0; i< ln ;i++ ) {
+
+            fullArr[i] = i; 
+            for (int j = 0; j < ln ; j++ ) {
+
+                if(array[i] == fullArr[j])
+                    count++;
+                
+            }
+
+            if (count == 0) {
+                return array[i];
+                
+            }
+               
+           }           
+       
+           return -1;
+        }
+    
+
+        /** Returns the second largest number in the array*/
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+
+        int maxArray = max(array);
+        int secMax = 0;
+        int n = array.length;
+
+        int[] newArray = new int[n];
+         
+        for (int i = 0; i < n ; i++ ) {
+
+            if(array[i] != maxArray)
+            {
+                newArray[i] = array[i];
+            }
+           
+        }
+        
+        return max(newArray);
+            
+
     }
+        /** Returns true if both arrays contain the same numbers */ 
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+
+         int ln1 = array1.length;
+         int ln2 = array2.length;
+
+         for (int i = 0; i < ln1; i++ ) {
+
+            int count = 0;
+
+            for (int j = 0; j < ln2 ;j++ ) {
+
+                if (array1[i] == array2[j]) {
+                    count++;
+                
+            }
+
+            }
+
+            if(count == 0)                
+                return false;
+
+
+         }
+
+       return true;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+
+
+        int ln = array.length;
+        int min1 = array[0];
+        int max1 = array[0];
+
+        if(array[0] <= array[ln - 1])
+        {
+            for (int i = 1; i < ln ; i++ ) {
+
+            if(array[i] >= min1)
+                min1 = array[i];
+
+            else
+                return false;
+            
+            }
+        }
+
+        else if(array[0] >= array[ln - 1]) {
+
+            for (int i = 1; i < ln ; i++ ) {
+
+            if(array[i] <= max1)
+                max1 = array[i];
+
+            else
+                return false;
+            
+            }
+
+        }
+
+        return true;
     }
 
+//}
+    /** Returns the sum of the elements of the array */ 
+    public static int sum(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+        }
+        return sum;
+    }
+
+    /** Returns the largest number from the elements of the array */ 
+    public static int max(int[] arr) {
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            
+            if(arr[i] > max)
+                max = arr[i];
+        }
+        return max;
+
+
+    }
+
+     /** Returns the largest number from the elements of the array */ 
+    public static int min(int[] arr) {
+         int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            
+            if(arr[i] < min)
+                min = arr[i];
+        }
+        return min;
+
+
+    }
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,151 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+
+        String vow = "yellow";
+        System.out.println(capVowelsLowRest(vow));
+        String inputString = "MMMM";
+        char targetChar = 'M';
+
+        int[] result = allIndexOf(inputString, targetChar);
+
+        System.out.println("Indexes of '" + targetChar + "' in '" + inputString + "':");
+        for (int index : result) {
+            System.out.print(index + " ");
+        }
     }
+
+    /*The function returns a string in which all the English vowels (a,e,i,o,u) in the original strings
+     are changed to uppercase, and all the other characters are changed to lowercase */
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
-    }
 
+        char[] vowels = {'a', 'i', 'o', 'u', 'e'};
+        int ln = string.length();
+        String result = "";
+
+        
+        for (int i = 0; i < ln ; i++ ) {
+
+            Boolean upper = isUpper(string.charAt(i));
+            Boolean vowel = false;
+
+            for (int j = 0; j < 5 ; j++ ) {
+            
+                if(string.charAt(i) == vowels[j] || string.charAt(i) == vowels[j] - 32) {
+
+                    vowel = true;
+
+                    if (!upper)
+                        result += (char)(string.charAt(i) - 32);
+                    else
+                        result += string.charAt(i);
+
+                }
+
+             }
+
+         if (isUpper(string.charAt(i)) && !vowel)
+            {
+
+                result += (char)(string.charAt(i) + 32);
+            }
+
+                else if (!vowel && !upper)
+                    result += string.charAt(i);
+
+            }
+    
+
+            return result;
+    }
+   /* 
+    The function returns a string in which the original string is modified according to three rules:
+    1. The first word is changed to lowercase (if it’s not lowercase already).
+    2. The first letter of each word is changed to uppercase (if it’s not uppercase already), and all the remaining letters in the word are changed to lowercase (if they are not lowercase already).
+    3. All the spaces (if any) are removed.
+*/
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        
+        String result = "";
+
+       if (string.charAt(0) >= 65 && string.charAt(0) <= 90) {
+
+            result += (char)(string.charAt(0) + 32);
+            
+        }
+        else
+            result += string.charAt(0);
+
+        for (int i = 1; i < string.length() ; i++ ) {
+
+            if(string.charAt(i) == ' ')
+            {
+                while (i + 1 < string.length() && string.charAt(i + 1) == ' ') {
+                     i++; 
+                 }
+             }
+
+            if (i + 1 < string.length()  && string.charAt(i) == ' ') {
+
+                 result += Character.toUpperCase(string.charAt(i + 1));
+                 i++; 
+            }
+            else
+                {
+                     if (isUpper(string.charAt(i))) 
+                    result += (char)(string.charAt(i) + 32);
+               
+                     else
+                        result += string.charAt(i);
+
+                }            
+           
+                
+        }
+
+        return result;
     }
 
+//Returns an array containing all the indexes in which the character appears in the string.
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+
+        int size = 0;
+
+        for (int i = 0; i < string.length() ; i++ ) {
+
+            if (string.charAt(i) == chr) {
+
+                size++;
+            }
+        }
+
+        int[] result = new int[size];
+
+        int index = 0;
+
+        for (int j = 0; j < string.length() ;j++ ) {
+
+            if(string.charAt(j) == chr)
+
+                result[index++] = j;
+
+            
+        }
+
+        return result;
     }
+
+    /* Returns true if a given char is upper case and false otherwise */
+
+    public static boolean isUpper (char chr) {
+
+         if (chr >= 65 && chr <= 90)
+            return true;
+
+        else
+            return false;
+    }
+
+
 }
